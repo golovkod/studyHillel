7c7ce79c25da5c0308e303f2b6de2181a371e00a
Головко Даниил <wayneejst@gmail.com>
2021-03-29 12:35:26 +0300

    edit 7 hw

---
+++ configureDB.php
@@ -0,0 +1,7 @@
+<?php
+
+define('servername', '127.0.0.1');
+define('username', 'root');
+define('password', '');
+define('dbname', 'testDB');
+
--- index.php
+++ index.php
@@ -1,57 +1,56 @@
-
 <?php
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "testDB";
+require __DIR__ . '/configureDB.php';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(servername, username, password, dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Реализовать выборку всех заказов по одному из контрагентов". "<br>";
-$sql = "SELECT * FROM Orders WHERE id_contragents = 123";
+echo "Реализовать выборку всех заказов по одному из контрагентов" . "<br>";
+$sql = "SELECT * FROM Orders INNER JOIN Goods on Goods.id_goods=Orders.id_goods
+                    WHERE Orders.id_contragents=123";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " id_orders: " . $row["id_orders"]. " id_contragents " . $row["id_contragents"]. "<br>";
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"] . " id_orders: " . $row["id_orders"] .
+            " quantity: " . $row["quantity"] . " id_goods: " . $row["id_goods"] .
+            " name: " . $row["name"] . " id_contragents " . $row["id_contragents"] . "<br>";
     }
 } else {
     echo "0 results";
 }
 
 
-echo "Реализовать выборку задвоенных контрагентов". "<br>";
+echo "Реализовать выборку задвоенных контрагентов" . "<br>";
 $sql = "SELECT * FROM Contragents WHERE name_contragents IN
         (SELECT name_contragents  FROM Contragents GROUP BY name_contragents HAVING COUNT(name_contragents) > 1)";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " id_contragents: " . $row["id_contragents"]. " name_contragents " . $row["name_contragents"]. "<br>";
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"] . " id_contragents: " . $row["id_contragents"] . " name_contragents " . $row["name_contragents"] . "<br>";
     }
 } else {
     echo "0 results";
 }
 
-echo "Реализовать выборку всех оплаченных заказов". "<br>";
+echo "Реализовать выборку всех оплаченных заказов" . "<br>";
 $sql = "SELECT * FROM Payment WHERE status_payment = 1";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " id_orders: " . $row["id_orders"]. " status_payments " . $row["status_payment"]. "<br>";
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"] . " id_orders: " . $row["id_orders"] . " status_payments " . $row["status_payment"] . "<br>";
     }
 } else {
     echo "0 results";
 }
 
-echo "Реализовать выборку по сумме всех заказов на контрагентов". "<br>";
+echo "Реализовать выборку по сумме всех заказов на контрагентов" . "<br>";
 
 $sql = "SELECT Contragents.id, Contragents.name_contragents, SUM(Orders.quantity * Goods.price) AS
     allsum FROM Orders, Goods, Contragents
@@ -60,13 +59,12 @@ GROUP BY Orders.id_contragents";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " id_contragents: " . $row["name_contragents"]. " name_contragents " . $row["allsum"]. "<br>";
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"] . " id_contragents: " . $row["name_contragents"] . " name_contragents " . $row["allsum"] . "<br>";
     }
 } else {
     echo "0 results";
 }
-
 
 
 $conn->close();
